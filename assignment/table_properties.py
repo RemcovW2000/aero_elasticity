88de994ffefea5e291364b5036d10b53fb7da709
remcovanwoerkom <RemcovW2000@gmail.com>
2026-07-10 13:09:08 +0200

    fix iterative solver

---
+++ assignment/table_properties.py
@@ -0,0 +1,17 @@
+"""
+Parameters from table 1 of paper.
+"""
+omega_h = 50 # rad/s
+omega_alpha = 100 # rad/s
+omega_beta = 300 # rad/s
+mu = 40
+
+a = -0.4
+b = 1.0
+c = 0.6
+
+x_alpa = 0.2
+x_beta = 0.025
+
+r_alpha_sq = 0.25
+r_beta_sq = 0.00625
